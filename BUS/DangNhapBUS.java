d6244de2dc3b2b93fe0604e7b9335dfd47ced5ee
Lê Võ Bảo Trân <150998732+levobaotran@users.noreply.github.com>
2024-01-06 10:08:44 +0700

    Add files via upload

---
+++ BUS/DangNhapBUS.java
@@ -0,0 +1,82 @@
+package QuanLyPizza.BUS;
+
+import QuanLyPizza.DAO.DangNhapDAO;
+import QuanLyPizza.DTO.TaiKhoan;
+
+import java.io.*;
+
+public class DangNhapBUS {
+
+    private final static int EMPTY_ERROR = 1;
+    private final static int WRONG_ERROR = 2;
+    public static TaiKhoan taiKhoanLogin = null;
+
+    public TaiKhoan getTaiKhoanDangNhap(String user, String password, boolean selected) {
+        if (kiemTraDangNhap(user, password) == EMPTY_ERROR) {
+            return null;
+        }
+        TaiKhoan tk = new TaiKhoan();
+        tk.setTenDangNhap(user);
+        tk.setMatKhau(password);
+
+        DangNhapDAO dangNhapDAO = new DangNhapDAO();
+        TaiKhoan account = dangNhapDAO.dangNhap(tk);
+        taiKhoanLogin = account;
+
+        if (account == null) {
+        } else {
+            PhanQuyenBUS phanQuyenBUS = new PhanQuyenBUS();
+            phanQuyenBUS.kiemTraQuyen(account.getQuyen());
+            xuLyGhiNhoDangNhap(user, password, selected);
+//            new MyDialog("Vì tình hình dịch Covid phức tạp, cửa hàng chỉ thực hiện bán mang về!", MyDialog.INFO_DIALOG);
+        }
+        return account;
+    }
+
+    public String getTaiKhoanGhiNho() {
+        try {
+            FileInputStream fis = new FileInputStream("remember.dat");
+            String line;
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(fis))) {
+                line = br.readLine();
+            }
+            return line;
+        }catch (IOException e) {
+        }
+        return "";
+    }
+
+    private void xuLyGhiNhoDangNhap(String user, String password, boolean selected) {
+        try {
+            if (!selected) {
+                user = "";
+                password = "";
+            }
+            try (FileWriter fw = new FileWriter("remember.dat")) {
+                fw.write(user + " | " + password);
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    private int kiemTraDangNhap(String user, String password) {
+        user = user.replaceAll("\\s+", "");
+        password = password.replaceAll("\\s+", "");
+        int result = 0;
+
+        TaiKhoan tk = new TaiKhoan();
+        tk.setTenDangNhap(user);
+        tk.setMatKhau(password);
+
+        DangNhapDAO dangNhapDAO = new DangNhapDAO();
+        TaiKhoan account = dangNhapDAO.dangNhap(tk);
+
+        if (user.length() <= 0 || password.length() <= 0) {
+            result = EMPTY_ERROR;
+        } else if (account == null) {
+            result = WRONG_ERROR;
+        }
+        return result;
+    }
+
+}
